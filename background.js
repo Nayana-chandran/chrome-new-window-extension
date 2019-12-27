5ae1f7966352ee1db255fd4f7c99dd8064134166
Nayana Chandran G.P <nayanachandran.g@contus.in>
2019-12-27 15:24:24 +0530

    listen the created new window in extension toogle click

--- background.js
+++ background.js
@@ -1,15 +1,32 @@
 /**
  * Listens for the app launching then creates the window
- *
  */
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.windows.create({
-        type: 'panel',
-        url: "https://developer.mozilla.org/"
-    }, function (newWindow) {
-        // Uncomment and write code if need new custom UI
-        // chrome.tabs.executeScript(newWindow.tabs[0].id, {
-            // code: 'document.write("hello world");'
-        // });
-    });
+var ba = chrome.browserAction;
+
+// Function to open the chrome extension into new chrome window
+var windowNotOpenTitle = 'Open popup window';
+var windowIsOpenTitle = 'Popup window is already open. Click to focus popup.';
+var popupWindowId = false; //popupWindowId can be true, false, or the popup's window Id.
+ba.onClicked.addListener(function () {
+    let width= 1100;
+    let height= 650;
+    if(popupWindowId === false){
+        popupWindowId = true; //Prevent user pressing the button multiple times.
+        ba.setTitle({title:windowIsOpenTitle});
+        chrome.windows.create({ 
+            'url': 'https://github.com/', 
+            'type': 'panel',
+            'width': width,
+            'height': height,
+            'left': (screen.width/2) - (width/2),
+            'top': (screen.height/2) - (height/2),
+            'focused': true
+        },function(win){
+            popupWindowId = win.id;
+        });
+        return;
+    }else if(typeof popupWindowId === 'number'){
+        //The window is open, and the user clicked the button., Focus the window.
+        chrome.windows.update(popupWindowId,{focused:true});
+    }
 });
